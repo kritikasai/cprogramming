f59c68623acee62146d652fe5743029082decc13
kritikasai <https://github.com/kritikasai/cprogramming.git>
2018-09-06 08:31:10 +0530

    Recursive Mutex is used. A thread can be locked and unlocked multiple times in the same thread using recursive mutex

---
+++ Recursive Mutex/recur_mutex.c
@@ -0,0 +1,41 @@
+#define _GNU_SOURCE
+#include<stdio.h>
+#include<pthread.h>
+
+int count=0;
+pthread_mutex_t countMutex= PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
+ 
+void *incthread(void *arg)
+{
+  while(1)
+  {
+   pthread_mutex_lock(&countMutex);
+   count++;
+   printf("Inc: %d\n",count);
+   pthread_mutex_unlock(&countMutex);
+  }
+}
+void *decthread(void *arg)
+{
+  while(1)
+  {
+   pthread_mutex_lock(&countMutex);
+   pthread_mutex_lock(&countMutex);
+   count--;
+   printf("Dec: %d\n",count);
+   pthread_mutex_unlock(&countMutex);
+   pthread_mutex_unlock(&countMutex);
+  }
+}
+
+int main()
+{
+ pthread_t incid, decid;
+ pthread_create(&incid,NULL,incthread,NULL);
+ pthread_create(&decid,NULL,decthread,NULL);
+ pthread_join(incid,NULL);
+ pthread_join(decid,NULL);
+ pthread_mutex_destroy(&countMutex);
+ return 0;
+}
+
